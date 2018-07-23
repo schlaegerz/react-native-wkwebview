48ca1d81f352f72485d36d4886bc82e0c6254b2c
schlaegerz <schlaegerz@gmail.com>
2018-07-23 13:44:41 -0700

    Update RCTWKWebView.m

--- ios/RCTWKWebView/RCTWKWebView.m
+++ ios/RCTWKWebView/RCTWKWebView.m
@@ -138,11 +138,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
 
 - (void)setupPostMessageScript {
   if (_messagingEnabled) {
-    NSString *source=@"window.originalPostMessage = window.postMessage; window.postMessage = function (data) { window.webkit.messageHandlers.reactNative.postMessage(data); }";
-    WKUserScript *script = [[WKUserScript alloc] initWithSource:source
-                           injectionTime:WKUserScriptInjectionTimeAtDocumentEnd
-                                               forMainFrameOnly:_injectedJavaScriptForMainFrameOnly];
-    [_webView.configuration.userContentController addUserScript:script];
+ 
   }
 }
 
